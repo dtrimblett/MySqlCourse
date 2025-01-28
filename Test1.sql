9a6545f767b64ece3bab319102da5265a692cf5c
Debs <155703707+dtrimblett@users.noreply.github.com>
2025-01-28 16:10:57 +0000

    Afternoon Session

--- Test1.sql
+++ Test1.sql
@@ -1,23 +1,70 @@
+/*
+Foundation Recap Exercise
+ 
+Use the table PatientStay.  
+This lists 44 patients admitted to London hospitals over 5 days between Feb 26th and March 2nd 2024
+*/
 SELECT
-
-    TOP 25 p.TransactionDate
-
-    , p.Price
-
-    , p.PostCode
-
-    , p.PAON
-
-    , pt.PropertyTypeName
-
+    *
 FROM
-    PricePaidSW12 p
-LEFT JOIN PropertyTypeLookup pt ON p.PropertyType = pt.PropertyTypeCode
+    PatientStay ps
+;
 
-WHERE
-    p.Street = 'ORMELEY ROAD'
+/*
+1. List the patients -
+a) in the Oxleas or PRUH hospitals and
+b) admitted in February 2024
+c) only the Surgery wards
+ 
+2. Show the PatientId, AdmittedDate, DischargeDate, Hospital and Ward columns only, not all the columns.
+3. Order results by AdmittedDate (latest first) then PatientID column (high to low)
+4. Add a new column LengthOfStay which calculates the number of days that the patient stayed in hospital, inclusive of both admitted and discharge date.
+*/
 
-ORDER BY
-    p.TransactionDate DESC;
+-- Write the SQL statement here
+
+SELECT*
+FROM PatientStay ps
+WHERE ps.Hospital IN ('Oxleas','PRUH')
+    AND ps.AdmittedDate BETWEEN '2024-02-01' AND '2024-02-29'
+    AND ps.ward LIKE '%Surgery'
+
+SELECT
+    ps.PatientId
+    , ps.AdmittedDate
+    , ps.DischargeDate
+    , ps.Hospital
+    , ps.Ward
+    , DATEDIFF(DAY, ps.AdmittedDate, ps.DischargeDate)+1 AS LOS
+FROM PatientStay ps
+WHERE ps.Hospital IN ('oxleas', 'PRUH')
+    and ps.AdmittedDate BETWEEN '2024-02-01' AND '2024-02-29'
+    AND ps.Ward LIKE '%Surgery'
+order by ps.AdmittedDate DESC, PS.PatientId DESC
+
+SELECT ps.Hospital, COUNT(*) AS NumberofPatients, SUM(ps.Tariff) AS TotalTariff
+from PatientStay ps
+GROUP BY ps.Hospital
+
+SELECT ps.Hospital,
+    COUNT(*) AS NumberofPatients
+    , SUM(ps.Tariff) AS TotalTariff
+from PatientStay ps
+GROUP BY ps.Hospital
+HAVING COUNT(*) >=10
+ORDER BY PS.Hospital DESC
+
+
+
+/*
+5. How many patients has each hospital admitted? 
+6. How much is the total tarriff for each hospital?
+7. List only those hospitals that have admitted over 10 patients
+8. Order by the hospital with most admissions first
+*/
+ 
+-- Write the SQL statement here
+
+
 
     
